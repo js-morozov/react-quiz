dbc2c0622001c66bc0a4cff2c8e9a063a2e0c2a2
js_morozov <frontend.morozov@gmail.com>
2020-11-22 18:36:03 +0300

    QuizList

---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class QuizList extends Component {
+
+  render() {
+    return (
+      <h1>Список тестов</h1>
+    );
+  }
+}
+
+export default QuizList;
